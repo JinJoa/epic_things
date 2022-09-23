7c8053eff37169a161314f4abbe8319b025a4870
JinJoa <mitejoa@gmail.com>
2022-09-24 05:27:41 +0900

    Initial Commit

---
+++ epic_things/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  epic_things
+//
+//  Created by jinjoa on 2022/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ epic_things/epic_thingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  epic_thingsApp.swift
+//  epic_things
+//
+//  Created by jinjoa on 2022/09/24.
+//
+
+import SwiftUI
+
+@main
+struct epic_thingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

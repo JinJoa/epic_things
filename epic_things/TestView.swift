6505154f95f791db7511e36db19cb41c7b2b30b1
JinJoa <mitejoa@gmail.com>
2022-09-25 21:23:39 +0900

    0925

---
+++ epic_things/TestView.swift
@@ -0,0 +1,20 @@
+//
+//  TestView.swift
+//  epic_things
+//
+//  Created by jinjoa on 2022/09/25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestView()
+    }
+}
--- epic_things/mainView.swift
+++ epic_things/mainView.swift
@@ -5,9 +5,33 @@
 import SwiftUI
 
 struct mainView: View {
-    @State var selectedColor = "color"
-    var homes = ["우리집", "본가", "기타"]
-    var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+    
+//    @State var menu: [String] = ["맴버", "도어록", "키", "초대", "게스트 키", "설정","와이파이 브릿지", "히스토리",""]
+//    @State var menuIcon = ["person.2","entry.lever.keypad","key","person.crop.circle.badge.plus","clock","gearshape","wifi","list.clipboard"]
+
+    @State var columns: [GridItem] = Array(repeating: .init(.flexible()), count: 3)
+
+    private struct MenuList: Identifiable {
+        let menu: String
+        let menuIcon: String
+        var id: String { menu }
+    }
+    private let menuList: [MenuList] = [
+        MenuList(menu: "맴버", menuIcon: "person.2"),
+        MenuList(menu: "도어록", menuIcon: "entry.lever.keypad"),
+        MenuList(menu: "키", menuIcon: "key"),
+        MenuList(menu: "초대", menuIcon: "person.crop.circle.badge.plus"),
+        MenuList(menu: "게스트 키", menuIcon: "clock"),
+        MenuList(menu: "설정", menuIcon: "gearshape"),
+        MenuList(menu: "와이파이 브릿지", menuIcon: "wifi"),
+        MenuList(menu: "히스토리", menuIcon: "list.clipboard"),
+        MenuList(menu: "", menuIcon: "")
+    ]
+
+    
+    
+    @State var selectedlocation = "우리집"
+    var location = ["우리집", "회사", "큰집"]
     
     
     var body: some View {
@@ -23,15 +47,18 @@ struct mainView: View {
             .padding(.top,50)
             HStack{
                 Rectangle()
-                    .foregroundColor(Color("lightGray"))
+                    .foregroundColor(.white)
                     .cornerRadius(10)
                     .frame(width: 100, height: 100)
+                    .overlay(RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color("lightGray"), lineWidth: 2))
                     .padding(20)
                 
                 VStack{
 //                    Menu("우리집") {
 //                        Button("우리집"){}
-//                        Button("추가"){}
+//                        Button("회사"){}
+//                        Button("큰집"){}
 //                    }
 //                    .frame(width:200, height: 40)
 //                    .overlay(
@@ -39,13 +66,14 @@ struct mainView: View {
 //                            .stroke(Color("deepRed"), lineWidth: 3)
 //                    )
 //                    .foregroundColor(Color("deepRed"))
-                    Picker("집", selection: $selectedColor) {
-                           ForEach(homes, id: \.self) {
+                    
+                    Picker("우리집", selection: $selectedlocation) {
+                           ForEach(location, id: \.self) {
                              Text($0)
                            }
                          }
                     .frame(width:200, height: 40)
-                    .foregroundColor(Color("deepRed"))
+                    //.foregroundColor(Color("deepRed"))
                     .overlay(
                         RoundedRectangle(cornerRadius: 10)
                             .stroke(Color("deepRed"), lineWidth: 3)
@@ -65,8 +93,9 @@ struct mainView: View {
                     .offset(x: 0, y:-300)
                 Circle()
                     .fill(Color.white)
-                    .frame(width: 150, height: 150)
+                    .frame(width: 160, height: 160)
                     .offset(x: 0, y:-300)
+                    .shadow(radius: 10)
                 Circle()
                     .fill(Color("deepRed"))
                     .frame(width: 140, height: 140)
@@ -81,14 +110,20 @@ struct mainView: View {
                    //.frame(width: 50, height: 50)
                     .offset(x: 0, y:-300)
                 
-                LazyVGrid(columns: columns) {
-                    ForEach((0...8), id: \.self) { _ in
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(menuList) { menuList in
                         ZStack(){
                             Rectangle()
                                 .cornerRadius(10)
-                                .frame(width: 110, height: 110)
+                                .frame(width: 120, height: 120)
                                 .foregroundColor(.white)
-                            Image("icon_user")
+                            Image(systemName: menuList.menuIcon)
+                              .foregroundColor(Color("deepRed"))
+                              .font(.system(size: 30))
+                            Text(menuList.menu)
+                                .offset(x:0, y:30)
+                                .font(.system(size: 14))
+                                .foregroundColor(.gray)
                                 
                         }
                     }
